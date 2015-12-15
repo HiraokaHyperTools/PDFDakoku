8d0b22d13451f853b228aa9275e398c4f40a3adb
kenjiuno <h8mastre@gmail.com>
2015-12-15 15:10:55 +0900

    0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PDFDakoku {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "打刻内容";
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(12, 31);
+            this.tb.Multiline = true;
+            this.tb.Name = "tb";
+            this.tb.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb.Size = new System.Drawing.Size(497, 323);
+            this.tb.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "作成したい";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A4:595, 842",
+            "8A0:6731, 9525"});
+            this.comboBox1.Location = new System.Drawing.Point(168, 371);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(521, 418);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Unicode MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF打刻君";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFDakoku {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var rows = tb.Text.Replace("\r\n", "\n").Split('\n');
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "*.pdf|*.pdf";
+            sfd.FileName = rows[0] + ".pdf";
+            if (sfd.ShowDialog(this) != System.Windows.Forms.DialogResult.OK) return;
+
+            iTextSharp.text.io.StreamUtil.AddToResourceSearch(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "iTextAsian.dll"));
+
+            FontFactory.Register(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "msgothic.ttc"));
+            //FontFactory.Register(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "msmincho.ttc"));
+
+            using (var fs = sfd.OpenFile()) {
+                Document doc = new Document();
+
+                var wr1 = PdfWriter.GetInstance(doc, fs);
+
+                var A4 = new iTextSharp.text.Rectangle(0, 0, 595, 842);
+                if (comboBox1.Text.Length != 0) {
+                    var cols = comboBox1.Text.Split(':')[1].Split(',');
+
+                    A4 = new iTextSharp.text.Rectangle(0, 0, float.Parse(cols[0]), float.Parse(cols[1]));
+                }
+
+                var f1 = iTextSharp.text.FontFactory.GetFont("MS-UIGothic", BaseFont.IDENTITY_H, 24 / 595.0f * A4.Width);
+
+                doc.SetPageSize(A4);
+                doc.SetMargins(0, 0, 0, 0);
+                doc.Open();
+
+                int y = 1;
+                foreach (String row in rows) {
+                    float cx1 = f1.BaseFont.GetWidthPoint(" ", f1.Size);
+                    float cy1 = f1.BaseFont.GetAscentPoint(row, f1.Size) - f1.BaseFont.GetDescentPoint(row, f1.Size);
+
+                    ColumnText.ShowTextAligned(
+                        wr1.DirectContent,
+                        Element.ALIGN_LEFT,
+                        new Phrase(row, f1),
+                        30,
+                        A4.Top - 30 - cy1 * y * 2f,
+                        0
+                        );
+                    y++;
+                }
+
+                doc.Close();
+            }
+        }
+    }
+}
